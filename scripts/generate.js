3d90ae9193ae5319d19d5c1a10f15f635dd16067
Ray Deck <ray@raydeck.com>
2020-03-31 08:45:58 -0500

    Adding addToProject to imports

--- scripts/generate.js
+++ scripts/generate.js
@@ -6,7 +6,7 @@ const path = require("path");
 const fs = require("fs");
 const chalk = require("chalk");
 const prompts = require("prompts");
-const { generate } = require("../lib/generate");
+const { generate, addToProject } = require("../lib/generate");
 let projectName;
 
 const logoFileName = "bootsplash_logo";
